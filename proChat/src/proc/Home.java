1ae2c7737185dd90a6fe1477a25e301db6230001
Cody Swendrowski <cswendrowski@mail.com>
2014-02-17 00:32:50 -0600

    Creates basic files and structures.

---
+++ proChat/src/proc/Home.java
@@ -0,0 +1,17 @@
+package proc;
+
+/**
+ * @author Cody Swendrowski
+ *
+ */
+public class Home {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
