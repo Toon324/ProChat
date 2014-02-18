e992a938a8eb852888146fa40d868d1d37325da5
Cody Swendrowski <cswendrowski@mail.com>
2014-02-18 01:09:35 -0600

    Sets up basic windows and frames for input and chat display.

--- proChat/src/proc/Home.java
+++ proChat/src/proc/Home.java
@@ -1,17 +1,54 @@
 package proc;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 /**
  * @author Cody Swendrowski
- *
+ * 
  */
 public class Home {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	JFrame frame;
 
+	public Home() {
+		frame = new JFrame();
+		frame.setSize(400,600);
+		frame.setTitle("ProChat");
+		JPanel masterPanel = new JPanel();
+		masterPanel.setLayout(new BoxLayout(masterPanel, BoxLayout.PAGE_AXIS));
+		
+		JTextField chat = new JTextField("");
+		JTextField entry = new JTextField("");
+		
+		chat.setEditable(false);
+		//chat.setBackground(Color.LIGHT_GRAY);
+		
+		JButton send = new JButton("Send");
+		
+		JPanel entryPanel = new JPanel(new GridLayout(1,0));
+		
+		entryPanel.add(entry);
+		entryPanel.add(send,BorderLayout.EAST);
+		
+		masterPanel.add(chat);
+		masterPanel.add(entryPanel,BorderLayout.SOUTH);
+		
+		frame.add(masterPanel);
+	}
+
+	/**
+	 * 
+	 */
+	public void show() {
+		frame.setVisible(true);
 	}
 
 }
---
+++ proChat/src/proc/ProChat.java
@@ -0,0 +1,17 @@
+package proc;
+
+/**
+ * @author Cody
+ *
+ */
+public class ProChat {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Home home = new Home();
+		home.show();
+	}
+
+}
