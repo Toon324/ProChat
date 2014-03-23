0283f03abcbea628523df1ac6148d53d3a627d35
Cody Swendrowski <cswendrowski@mail.com>
2014-03-23 18:37:56 -0500

    VOIP now only opens when chat is established.

--- proChat/src/proc/Home.java
+++ proChat/src/proc/Home.java
@@ -345,7 +345,7 @@ public class Home implements ActionListener, MouseListener, KeyListener,
 					message.setType(Message.Type.headline);
 					System.out.println("Sent ip: " + message.getBody());
 					connection.getConnection().sendPacket(message);
-					new VoiceCall(msg.getBody());
+					//new VoiceCall(msg.getBody());
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
