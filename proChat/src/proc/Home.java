a8ca3b56c841c5a8873961d428f0c25f0a29cd3d
Cody Swendrowski <cody@swendrowski.us>
2014-03-24 20:59:18 -0500

    Home screen now shows version.

--- proChat/src/proc/Home.java
+++ proChat/src/proc/Home.java
@@ -102,7 +102,7 @@ public class Home implements ActionListener, MouseListener, KeyListener,
 
 		frame = new JFrame();
 		frame.setSize(400, 608);
-		frame.setTitle("ProChat");
+		frame.setTitle("ProChat v0.1.5 ALPHA");
 
 		JLabel direct = new JLabel("Directly contact this person:");
 
