9b2a74b3b9a6dce159f639b17363faeb968382dd
Cody Swendrowski <cswendrowski@hansondodge.com>
2014-08-20 10:17:08 -0500

    Commits the ProChat class changes.

--- proChat/src/proc/ProChat.java
+++ proChat/src/proc/ProChat.java
@@ -1,22 +1,45 @@
 package proc;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.concurrent.Executors;
+
+import javax.imageio.ImageIO;
+
 /**
  * @author Cody
  *
  */
 public class ProChat {
 
+	private static LoginWindow login;
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		try {
 			Log.createWriter(); //Create debugger
-			new LoginWindow();
+			login = new LoginWindow();
 		}
 		catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static BufferedImage loadImage(String imagePath) {
+		try {
+			return ImageIO.read(login.getClass().getResourceAsStream(
+					imagePath));
+		} catch (IOException e) {
+			Log.e("Could not load image " + imagePath + " due to " + e.getMessage());
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static void createNewThread(Runnable runnable) {
+		Executors.newCachedThreadPool().execute(runnable);	
 	}
 
 }
